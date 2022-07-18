23117028a61b3f1041e3e5b4b93669d4316745b7
Francesco-Vitolo <94391020+Francesco-Vitolo@users.noreply.github.com>
2022-07-18 22:36:43 +0200

    Add files via upload

---
+++ Email/Email/EmailHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+
+namespace Email
+{
+    public class EmailHelper
+    {
+        private string Sender { get; }
+        private string Password { get; }
+        public EmailHelper(string sender, string password)
+        {
+            Sender = sender;
+            Password = password;
+        }
+
+        public void SendMail(string empfaenger, string betreff, string nachricht)
+        {
+            NetworkCredential credentials = new NetworkCredential(Sender, Password);
+            SmtpClient client = new SmtpClient("smtp-mail.outlook.com")
+            {
+                Port = 587,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                EnableSsl = true,
+                Credentials = credentials,
+            };
+
+            try
+            {
+                MailMessage mail = new MailMessage(Sender, empfaenger)
+                {
+                    Subject = betreff,
+                    Body = nachricht
+                };
+                client.Send(mail);
+
+                Console.WriteLine("Email wurde an {0} gesendet",empfaenger);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+    }
+}
---
+++ Email/Email/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Email
+{
+    class Program
+    {
+        private const string EmailEmpfaenger = "example@example.de";
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"../../LoginCred.txt");
+            Console.WriteLine("Betreff eingeben");
+            string betreff = Console.ReadLine();
+            Console.WriteLine("Nachricht eingeben");
+            string nachricht = Console.ReadLine();
+
+            EmailHelper sender = new EmailHelper(lines[0], lines[1]);
+            sender.SendMail(EmailEmpfaenger, betreff, nachricht);
+        }
+    }
+}
